d1ebb2440e8c82a19258c740dcba5289a1c77667
Konrad Blaszyński <blaszynski.konrad@gmail.com>
2023-12-06 23:06:07 +0100

    Mobile Navbar Update
    Adding css and sliding mobile navbar

---
+++ index.js
@@ -0,0 +1,21 @@
+function toogleNavBar()
+{
+    const navElement = document.querySelector(".navbar");
+    const navMoreButtonElement = document.querySelector(".mobile-nav-more");
+    const body = document.querySelector("body");
+
+    console.log(navMoreButtonElement.attributes);
+
+    if(navElement.style.display === "none")
+    {
+        navElement.style.display = "flex";
+        body.style.overflow = "hidden";
+        navMoreButtonElement.innerHTML = "<path d='M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z'/>";
+    }
+    else
+    {
+        navElement.style.display = "none";
+        body.style.overflow = "auto";
+        navMoreButtonElement.innerHTML = "<path d='M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3'/>";
+    }
+}
